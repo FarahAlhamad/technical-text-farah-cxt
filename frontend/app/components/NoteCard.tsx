1b3f5d7ddcea66eca3b7df24fcb720a1b7bfc261
Farah Alhamad <farah.alhamad9@gmail.com>
2025-02-06 01:04:57 +0200

    card UI fix

--- frontend/app/components/NoteCard.tsx
+++ frontend/app/components/NoteCard.tsx
@@ -4,8 +4,12 @@ import { Note } from "../services/notesService";
 const NoteCard = ({ note }: { note: Note }) => {
   return (
     <div className="bg-blue-50 md:max-w-2xl min-h-[120px] shadow-md rounded-md p-4">
-      <h2 className="font-semibold mb-1">{note.title}</h2>
-      <p className="text-sm max-h-[200px] overflow-y-auto">{note.content}</p>
+      <h2 className="font-semibold mb-1 line-clamp-2" title={note.title}>
+        {note.title}
+      </h2>
+      <p className="text-sm max-h-[200px] overflow-y-auto overflow-x-hidden">
+        {note.content}
+      </p>
     </div>
   );
 };
